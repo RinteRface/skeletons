2b0feb313ccc4b3d86e9a7b4f75bec5fb8dc7d5d
DivadNojnarg <dgranjon@ymail.com>
2020-07-02 09:27:52 +0200

    add css deps

---
+++ R/deps.R
@@ -0,0 +1,6 @@
+skeletons_css <- htmltools::htmlDependency(
+  name = "skeleton-elements",
+  version = "0.9.0",
+  src = c(href = "https://unpkg.com/@skeleton-elements/core/"),
+  stylesheet = "skeleton-elements.css"
+)
